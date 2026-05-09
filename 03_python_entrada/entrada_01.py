eb0afb3a36fdf20cad98878f722e2b5249147eed
2pac <eduardacaroline0616@gmail.com>
2026-05-09 15:00:25 -0300

    entradas em python

---
+++ 03_python_entrada/entrada_01.py
@@ -0,0 +1,10 @@
+# autor: duda caroline
+# projeto: entradas em python
+
+# declarar as variaveis 
+valor1 = 6
+valor2 = 2
+soma = valor1 + valor2
+
+# exibir resultados 
+print ("a soma é:" , soma)
---
+++ 03_python_entrada/entrada_03.py
@@ -0,0 +1,10 @@
+# autor :dudinha]
+# projeto: entrada com input 
+
+# declarção de variaveis 
+valor1 = int(input("digite o primeiro valor: "))
+valor2 = int(input("digite o segundo valor: "))
+soma = valor1 + valor1
+
+# exibindo os resultados
+print ("o resultado da soma é: ",soma)
